8b8d493bd7d0cd0ff0297f95840b10b1b0033038
MedAbdelmoula <166121770+MedAbdelmoula@users.noreply.github.com>
2024-04-05 14:16:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,9 +12,14 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be :', name_on_order)
 
+try:
 
-cnx = st.connection("snowflake")
-session = cnx.session()
+    cnx = st.connection("snowflake")
+    session = cnx.session()
+except Exception as e:
+    st.warning(f"Error while trying to connect: {e}")
+
+
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
